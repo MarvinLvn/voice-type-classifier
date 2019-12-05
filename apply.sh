3f7a3b547a15f39aa898a9f72c263c7e56659628
MarvinLvn <marvin.lavechin@ensimag.grenoble-inp.fr>
2019-12-05 17:43:48 +0100

    Add minimal duration of 0.1s for speech utterances

--- apply.sh
+++ apply.sh
@@ -2,6 +2,12 @@
 THISDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 declare -a classes=(KCHI CHI MAL FEM SPEECH)
+declare -A folders=(
+["KCHI"]="model/train/BBT_emp.SpeakerDiarization.All.train/validate_KCHI/BBT_emp.SpeakerDiarization.All.development/apply/0455"
+["CHI"]="model/train/BBT_emp.SpeakerDiarization.All.train/validate_CHI/BBT_emp.SpeakerDiarization.All.development/apply/0265"
+["MAL"]="model/train/BBT_emp.SpeakerDiarization.All.train/validate_MAL/BBT_emp.SpeakerDiarization.All.development/apply/0330"
+["FEM"]="model/train/BBT_emp.SpeakerDiarization.All.train/validate_FEM/BBT_emp.SpeakerDiarization.All.development/apply/0495"
+["SPEECH"]="model/train/BBT_emp.SpeakerDiarization.All.train/validate_SPEECH/BBT_emp.SpeakerDiarization.All.development/apply/0495")
 
 if [ $# -ge 2 ]; then
     declare -a classes=() # empty array
@@ -12,6 +18,7 @@ fi;
 if [ "$(ls -A $1/*.wav)" ]; then
     echo "Found wav files."
 
+    bn=$(basename $1)
     echo "Creating config for pyannote."
     # Create pyannote_tmp_config containing all the necessary files
     rm -rf pyannote_tmp_config
@@ -19,24 +26,40 @@ if [ "$(ls -A $1/*.wav)" ]; then
 
     # Create database.yml
     echo "Databases:
-    MySet: $1/{uri}.wav
+    $bn: $1/{uri}.wav
 
 Protocols:
-  MySet:
+  $bn:
     SpeakerDiarization:
       All:
         test:
-          annotated: $THISDIR/pyannote_tmp_config/my_set.uem" >> $THISDIR/pyannote_tmp_config/database.yml
+          annotated: $THISDIR/pyannote_tmp_config/$bn.uem" >> $THISDIR/pyannote_tmp_config/database.yml
+
 
     # Create .uem file
     for audio in $1/*.wav; do
         duration=$(soxi -D $audio)
         echo "$(basename ${audio/.wav/}) 1 0.0 $duration"
-    done >> $THISDIR/pyannote_tmp_config/my_set.uem
+    done >> $THISDIR/pyannote_tmp_config/$bn.uem
     echo "Done creating config for pyannote."
 
     export PYANNOTE_DATABASE_CONFIG=pyannote_tmp_config/database.yml
-    pyannote-multilabel apply --subset=test --gpu model/train/BBT_emp.SpeakerDiarization.All.train/validate_FEM/BBT_emp.SpeakerDiarization.All.development MySet.SpeakerDiarization.All
+
+    OUTPUT=output_voice_type_classifier/$bn/
+    mkdir -p output_voice_type_classifier/$bn/
+
+    for class in ${classes[*]}; do
+        echo "Extracting $class"
+        awk -F' ' -v var="$class" 'BEGIN{OFS = "\t"}{print $1,$2,$3,$4,$5,$6,$7,var,$9,$10}' ${folders[$class]}/MySet.SpeakerDiarization.All.test.rttm \
+            > $OUTPUT/$class.rttm
+        #echo ${folders[$class]}
+        #pyannote-multilabel apply --subset=test --gpu model/train/BBT_emp.SpeakerDiarization.All.train/validate_$class/BBT_emp.SpeakerDiarization.All.development $bn.SpeakerDiarization.All
+    done;
+    cat $OUTPUT/{KCHI,CHI,MAL,FEM,SPEECH}.rttm > $OUTPUT/all.rttm
+
+    # Super powerful sorting bash command :D !
+    # Sort alphabetically to the second column, and numerically to the fourth one.
+    sort -b -k2,2 -k4,4n $OUTPUT/all.rttm >> $OUTPUT/all.rttm
 else
     echo "The folder you provided doesn't contain any wav files."
 fi;
