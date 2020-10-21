4db2708b1adaa7889d4e385457a766080f962554
Sarp Uner <sarpim@gmail.com>
2020-10-20 20:40:37 -0400

    Fixed resolution of THISDIR so it can correctly find the model.

--- apply.sh
+++ apply.sh
@@ -2,17 +2,7 @@
 
 start=`date +%s`
 
-# check if script is started via SLURM or bash
-# if with SLURM: there variable '$SLURM_JOB_ID' will exist
-# `if [ -n $SLURM_JOB_ID ]` checks if $SLURM_JOB_ID is not an empty string
-if [[ -n $SLURM_JOB_ID ]]; then
-    # check the original location through scontrol and $SLURM_JOB_ID
-    THISDIR=$(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}' | sed -e 's/\s.*$//')
-    THISDIR=$(dirname $THISDIR)
-else
-    # otherwise: started with bash. Get the real location.
-    THISDIR="$( cd "$( dirname "$0" )" && pwd )"
-fi
+THISDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 hn=$(hostname)
 if [[ $hn == "puck4.cluster" ]]; then
